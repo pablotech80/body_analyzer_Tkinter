89be75c3ee1fa41715b599a3d9c46ba6ec25091e
pablotech80 <ptecherasosa@icloud.com>
2024-09-05 17:46:16 +0200

    rename login

--- main.py
+++ main.py
@@ -574,7 +574,7 @@ class LoginWindow:
                     root (tk.Tk): La ventana principal de la aplicación.
                 """
         self.root = root
-        self.root.title("Welcome this is CoachBodyMetrics")
+        self.root.title("Welcome to CoachBodyMetrics")
         self.setup_ui()
 
     def setup_ui(self):
